94cad19728332dbb09eec0238ab7c107dc728165
Saurabh Raj <113858096+SaurabhRaj-SR31@users.noreply.github.com>
2024-06-28 22:13:54 +0530

    Time: 239 ms (90.52%), Space: 116.2 MB (97.24%) - LeetHub

---
+++ 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long maximumImportance(int n, vector<vector<int>>& roads) {
+         vector<int> degreeCounts(n, 0);
+
+        // Iterate through each road to increment the degree of the two cities connected by the road
+        for (const auto& road : roads) {
+            ++degreeCounts[road[0]];
+            ++degreeCounts[road[1]];
+        }
+
+        // Sort the degree counts in non-decreasing order
+        sort(degreeCounts.begin(), degreeCounts.end());
+
+        // Initialize a variable to store the maximum importance sum
+        long long maxImportance = 0;
+
+        // Assign the importance to each city based on its degree count
+        // The city with the smallest degree gets an importance of 1, the next one gets 2, and so on
+        for (int i = 0; i < n; ++i) {
+            maxImportance += static_cast<long long>(i + 1) * degreeCounts[i];
+        }
+
+        // Return the calculated maximum importance sum
+        return maxImportance;
+    }
+};
