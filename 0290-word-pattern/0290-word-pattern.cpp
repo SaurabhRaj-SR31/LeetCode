c83ebc9633a8c89cacffcdac4ef3878c778a0914
Saurabh Raj <113858096+SaurabhRaj-SR31@users.noreply.github.com>
2024-07-17 10:25:25 +0530

    Time: 0 ms (100%), Space: 8.2 MB (7.38%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        unordered_map<char,int>chartoidx;
-        unordered_map<string,int>stringtoidx;
-        int count=0;
-        int i=0;
+        unordered_map<string,char>mp;
+        set<char>st;
+        int n=pattern.size();
         stringstream ss(s);
         string token;
-        int n=pattern.size();
+        int count=0;
+        vector<string>vec;
+
         while(ss>>token){
             count++;
-            if(i==n || chartoidx[pattern[i]]!=stringtoidx[token])return false;
-            chartoidx[pattern[i]]=i+1;
-            stringtoidx[token]=i+1;
-            i++;
-        }
-        if(count != n || i!=n)return false;
-        return true;
+            vec.push_back(token);
 
+        }
+        if(count!=n)return false;
+        for(int i=0;i<n;i++){
+            if(mp.find(vec[i])==mp.end()&&st.find(pattern[i])==st.end()){
+                mp[vec[i]]=pattern[i];
+                st.insert(pattern[i]);
+            }
+            else if(mp[vec[i]]!=pattern[i])return false;
+        }
+return true;
     }
 };
