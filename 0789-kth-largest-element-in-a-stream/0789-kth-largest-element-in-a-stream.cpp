1f617df8bef6f605bb3c228f245e2ea1c6bacd83
Saurabh Raj <113858096+SaurabhRaj-SR31@users.noreply.github.com>
2024-08-27 00:29:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.cpp
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.cpp
@@ -1,23 +1,11 @@
 class KthLargest {
-public:  int K;
-    priority_queue<int, vector<int>, greater<int>> pq;
-    
+public:
     KthLargest(int k, vector<int>& nums) {
-        K = k; // Move the initialization of K outside the loop
-        for(auto &n : nums){
-            pq.push(n);
-            if(pq.size() > K){
-                pq.pop();
-            }
-        }
+        
     }
     
     int add(int val) {
-        pq.push(val);
-        if(pq.size() > K){
-            pq.pop();
-        }
-        return pq.top();
+        
     }
 };
 
