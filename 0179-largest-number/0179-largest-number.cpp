b87392c3f2230def88203648b50bf2624f5672f5
Saurabh Raj <113858096+SaurabhRaj-SR31@users.noreply.github.com>
2024-09-18 22:08:20 +0530

    Time: 0 ms (100%), Space: 15.6 MB (85.32%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string largestNumber(vector<int>& nums) {
-        auto lambda=[&](int a,int b){
+           auto lambda=[&](int a,int b){
             string s1=to_string(a);
             string s2=to_string(b);
             return (s1+s2)>(s2+s1);
@@ -14,5 +14,6 @@ public:
             res+=to_string(i);
         }
         return res;
+    
     }
 };
