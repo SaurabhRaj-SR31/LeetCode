508b19e24fbe7f148aa1453e84b71e85fb9d067c
Saurabh Raj <113858096+SaurabhRaj-SR31@users.noreply.github.com>
2025-02-10 13:40:31 +0530

    Time: 0 ms (100%), Space: 8.6 MB (56.73%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string result = "";
+
+        for(char &ch : s) {
+            if(ch >= 'a' && ch <= 'z') {
+                result.push_back(ch);
+            } else if(!result.empty()) {
+                result.pop_back();
+            }
+        }
+
+        return result;
+    }
+};
