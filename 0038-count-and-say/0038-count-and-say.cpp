4e6a7040ff465cd4a2c8bd61b21e677f2c0b8408
Saurabh Raj <113858096+SaurabhRaj-SR31@users.noreply.github.com>
2025-04-18 18:48:05 +0530

    Time: 3 ms (83.88%), Space: 10 MB (61.25%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
-    string countAndSay(int n) {
-        string res="";
-        if(n==1)return "1";
-        string say=countAndSay(n-1);
-        for(int i=0;i<say.size();i++)
-        { char ch=say[i];
-            int cnt=1;
-            while(i<say.size()-1 && say[i]==say[1+i])
-            {
-                cnt++;i++;
+       string countAndSay(int n) {
+        if(n == 1)
+            return "1";
+        
+        
+        string say = countAndSay(n-1);
+        
+        string result = "";
+        
+
+        for(int i = 0; i<say.length(); i++) {
+            
+            int count = 1;
+            char ch = say[i];
+            
+            while(i < say.length()-1 && say[i] == say[i+1]) {
+                count++;
+                i++;
             }
-            res+=to_string(cnt)+string(1,ch);
+            
+            result += to_string(count) + string(1, say[i]);
+            
         }
-        return res;
+        
+        return result;
+        
     }
 };
